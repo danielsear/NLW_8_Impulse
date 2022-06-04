3f116e8d28acda34ec7532d62a9dd4457f7fe8cf
Danielsear <danielandradeseara@hotmail.com>
2022-06-04 07:51:29 -0300

    created CloseBttun component

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,7 +1,19 @@
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] text-white md:w-auto">
-      Hello Word
+      <header>
+        <span className="text-xl leading-6 ">Deixe seu feedback</span>
+      </header>
+      <p>Hello Word</p>
+      <footer className="text-xs text-neutral-400">
+        Feito com amor pela{' '}
+        <a
+          className="underline underline-offset-2"
+          href="https://rocketseat.com.br"
+        >
+          RocketSeat
+        </a>
+      </footer>
     </div>
   )
 }
