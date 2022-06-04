fe49b5870ca499c186aeaea94330a9f4e6aa1181
Danielsear <danielandradeseara@hotmail.com>
2022-06-04 12:13:08 -0300

    created FeedbackSucessStep component, thus terminating every part of the application's Feedback Widget

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -6,6 +6,7 @@ import thoughtImageUrl from '../../assets/thought.svg'
 
 import { FeedbackTypeStep } from './Steps/FeedbackTypeStep'
 import { FeedbackContentStep } from './Steps/FeedbackContentStep'
+import { FeedbackSucessStep } from './Steps/FeedbackSucessStep'
 
 export const feedbackTypes = {
   BUG: {
@@ -52,20 +53,31 @@ export type FeedbackType = keyof typeof feedbackTypes
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackSend, setFeedbackSend] = useState(false)
 
   function handleRestartFeedback() {
+    setFeedbackSend(false)
     setFeedbackType(null)
   }
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] text-white md:w-auto">
-      {!feedbackType ? (
-        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
-      ) : (
-        <FeedbackContentStep
-          feedbackType={feedbackType}
+      {feedbackSend ? (
+        <FeedbackSucessStep
           onFeedbackRestartRequested={handleRestartFeedback}
         />
+      ) : (
+        <>
+          {!feedbackType ? (
+            <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+          ) : (
+            <FeedbackContentStep
+              feedbackType={feedbackType}
+              onFeedbackRestartRequested={handleRestartFeedback}
+              onFeedbackSend={() => setFeedbackSend(true)}
+            />
+          )}
+        </>
       )}
 
       <footer className="text-xs text-neutral-400">
