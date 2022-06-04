d3a82b2f58803bdb1733c7fb7c683b3ddaec8d4e
Danielsear <danielandradeseara@hotmail.com>
2022-06-04 09:08:08 -0300

    structuring the code, creating components for each page's functionality, improving code visualization, facilitating future maintenance. creating the FeedbackTypeStep component

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,11 +1,12 @@
-import { CloseButton } from './CloseButton'
+import { CloseButton } from '../CloseButton'
 
-import bugImageUrl from '../assets/bug.svg'
-import ideaImageUrl from '../assets/idea.svg'
-import thoughtImageUrl from '../assets/thought.svg'
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
 import { useState } from 'react'
+import { FeedbackTypeStep } from './Steps/FeedbackTypeStep'
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: 'Problema',
     image: {
@@ -28,7 +29,7 @@ const feedbackTypes = {
     }
   }
 }
-type FeedbackType = keyof typeof feedbackTypes
+export type FeedbackType = keyof typeof feedbackTypes
 
 /*
 //Object.entries(feedbackType) => 
@@ -57,21 +58,11 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full">
-        {Object.entries(feedbackTypes).map(([key, value]) => {
-          return (
-            <button
-              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-              key={key}
-              type="button"
-              onClick={() => setFeedbackType(key as FeedbackType)}
-            >
-              <img src={value.image.source} alt={value.image.alt} />
-              <span>{value.title}</span>
-            </button>
-          )
-        })}
-      </div>
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <p>Hello world</p>
+      )}
 
       <footer className="text-xs text-neutral-400">
         Feito com amor pela{' '}
